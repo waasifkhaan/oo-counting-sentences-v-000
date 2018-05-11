7f53b9215cadcbc0c9852e720e8bfcd1386350d6
Wasif Khan <waasif.khaan@gmail.com>
2018-05-11 05:03:17 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,11 @@ require 'pry'
 class String
 
   def sentence?
+<<<<<<< HEAD
     self.end_with?(".")
+=======
+    self.end_with?("?")
+>>>>>>> 585c77aeed03b95de2c7f3a00fb5fec4f1425645
     
   end
 
